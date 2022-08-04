24ce6586268007c0504dbedfb531ed69fe1e656f
Sudiksha <sudiksharavle2839@gmail.com>
2022-08-04 10:42:14 +0530

    git init

---
+++ Main1.java
@@ -0,0 +1,39 @@
+class Polygon
+ {
+ public void render() 
+ {
+    System.out.println("Rendering Polygon...");
+  }
+}
+
+class Square extends Polygon 
+{
+
+  public void render() 
+  {
+    System.out.println("Rendering Square...");
+  }
+}
+
+class Circle extends Polygon {
+
+ 
+  public void render() {
+    System.out.println("Rendering Circle...");
+  }
+}
+
+class Main1
+{
+  public static void main(String[] args) 
+  {
+    
+  
+    Square s1 = new Square();
+    s1.render();
+
+    
+    Circle c1 = new Circle();
+    c1.render();
+  }
+}
